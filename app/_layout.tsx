50d8bcbd4316ee47eab79f39b8a7defbe97c5a0d
Nnanyelugo Tech <nnanyelugomeputaife@gmail.com>
2026-02-17 14:51:43 +0100

    refactor: Remove font loading and splash screen management from RootLayout.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,8 @@ import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
-
-import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete
 SplashScreen.preventAutoHideAsync();
@@ -16,25 +14,7 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    "Lato-Black": require("../assets/fonts/Lato-Black.ttf"),
-    "RobotoCondensed-Italic-VariableFont_wght": require("../assets/fonts/RobotoCondensed-Italic-VariableFont_wght.ttf"),
-  });
-
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  // Prevents app UI from rendering before fonts ready
-  if (!loaded) {
-    return null;
-  }
+  
 
   return (
     <SafeAreaProvider>
